6955c41770eb231c1fa72ff8d7889b06d305a424
shhan <shhan0226@gmail.com>
2023-03-15 11:46:16 +0900

    update swift 03.15

--- ObjectStorage/swift-storage-dy.sh
+++ ObjectStorage/swift-storage-dy.sh
@@ -52,34 +52,30 @@ crudini --set /etc/rsyncd.conf "" "log file" /var/log/rsyncd.log
 crudini --set /etc/rsyncd.conf "" "pid file" /var/run/rsyncd.pid
 crudini --set /etc/rsyncd.conf "" address ${SET_IP}
 
-
 # account
-crudini --set /etc/rsyncd.conf account path /srv/node
-crudini --set /etc/rsyncd.conf account "read only" false
-crudini --set /etc/rsyncd.conf account "write only" no
 crudini --set /etc/rsyncd.conf account "max connections" 25
+crudini --set /etc/rsyncd.conf account path /srv/node/
+crudini --set /etc/rsyncd.conf account "read only" false
 crudini --set /etc/rsyncd.conf account "lock file" /var/lock/account.lock
-crudini --set /etc/rsyncd.conf account list yes
-#crudini --set /etc/rsyncd.conf account "incoming chmod" 0644
-#crudini --set /etc/rsyncd.conf account "outgoing chmod" 0644
+#crudini --set /etc/rsyncd.conf account "write only" no
+#crudini --set /etc/rsyncd.conf account list yes
+
 # container
-crudini --set /etc/rsyncd.conf container path /srv/node
-crudini --set /etc/rsyncd.conf container "read only" false
-crudini --set /etc/rsyncd.conf container "write only" no
 crudini --set /etc/rsyncd.conf container "max connections" 25
+crudini --set /etc/rsyncd.conf container path /srv/node/
+crudini --set /etc/rsyncd.conf container "read only" false
 crudini --set /etc/rsyncd.conf container "lock file" /var/lock/account.lock
-crudini --set /etc/rsyncd.conf container list yes
-#crudini --set /etc/rsyncd.conf container "incoming chmod" 0644
-#crudini --set /etc/rsyncd.conf container "outgoing chmod" 0644
+#crudini --set /etc/rsyncd.conf container "write only" no
+#crudini --set /etc/rsyncd.conf container list yes
+
 # object
-crudini --set /etc/rsyncd.conf object path /srv/node
-crudini --set /etc/rsyncd.conf object "read only" false
-crudini --set /etc/rsyncd.conf object "write only" no
 crudini --set /etc/rsyncd.conf object "max connections" 25
+crudini --set /etc/rsyncd.conf object path /srv/node/
+crudini --set /etc/rsyncd.conf object "read only" false
 crudini --set /etc/rsyncd.conf object "lock file" /var/lock/account.lock
-crudini --set /etc/rsyncd.conf object list yes
-#crudini --set /etc/rsyncd.conf object "incoming chmod" 0644
-#crudini --set /etc/rsyncd.conf object "outgoing chmod" 0644
+#crudini --set /etc/rsyncd.conf object "write only" no
+#crudini --set /etc/rsyncd.conf object list yes
+
 # swift_server
 # crudini --set /etc/rsyncd.conf swift_server path /etc/swift
 # crudini --set /etc/rsyncd.conf swift_server "read only" true
@@ -89,9 +85,11 @@ crudini --set /etc/rsyncd.conf object list yes
 # crudini --set /etc/rsyncd.conf swift_server "outgoing chmod" 0644
 # crudini --set /etc/rsyncd.conf swift_server "max connections" 25
 # crudini --set /etc/rsyncd.conf swift_server "lock file" /var/lock/account.lock
+
 # /etc/default/rsync
 crudini --set /etc/default/rsync "" RSYNC_ENABLE true
 service rsync start
+
 ##################################
 # server config
 ##################################
@@ -99,6 +97,7 @@ apt-get install swift swift-account swift-container swift-object xfsprogs -y
 curl -o /etc/swift/account-server.conf https://opendev.org/openstack/swift/raw/branch/stable/${OPENSTACK_VER}/etc/account-server.conf-sample
 curl -o /etc/swift/container-server.conf https://opendev.org/openstack/swift/raw/branch/stable/${OPENSTACK_VER}/etc/container-server.conf-sample
 curl -o /etc/swift/object-server.conf https://opendev.org/openstack/swift/raw/branch/stable/${OPENSTACK_VER}/etc/object-server.conf-sample
+
 # /etc/swift/account-server.conf
 crudini --set /etc/swift/account-server.conf DEFAULT bind_ip ${SET_IP}
 crudini --set /etc/swift/account-server.conf DEFAULT bind_port 6202
@@ -109,6 +108,7 @@ crudini --set /etc/swift/account-server.conf DEFAULT mount_check True
 crudini --set /etc/swift/account-server.conf pipeline:main pipeline "healthcheck recon account-server"
 crudini --set /etc/swift/account-server.conf filter:recon use egg:swift#recon
 crudini --set /etc/swift/account-server.conf filter:recon recon_cache_path /var/cache/swift
+
 # /etc/swift/container-server.conf
 crudini --set /etc/swift/container-server.conf DEFAULT bind_ip ${SET_IP}
 crudini --set /etc/swift/container-server.conf DEFAULT bind_port 6201
@@ -119,6 +119,7 @@ crudini --set /etc/swift/container-server.conf DEFAULT mount_check True
 crudini --set /etc/swift/container-server.conf pipeline:main pipeline "healthcheck recon container-server"
 crudini --set /etc/swift/container-server.conf filter:recon use egg:swift#recon
 crudini --set /etc/swift/container-server.conf filter:recon recon_cache_path /var/cache/swift
+
 # /etc/swift/object-server.conf
 crudini --set /etc/swift/object-server.conf DEFAULT bind_ip ${SET_IP}
 crudini --set /etc/swift/object-server.conf DEFAULT bind_port 6200
@@ -130,13 +131,14 @@ crudini --set /etc/swift/object-server.conf pipeline:main pipeline "healthcheck 
 crudini --set /etc/swift/object-server.conf filter:recon use egg:swift#recon
 crudini --set /etc/swift/object-server.conf filter:recon recon_cache_path /var/cache/swift
 crudini --set /etc/swift/object-server.conf filter:recon recon_lock_path /var/lock
+
 # ownership
 chown -R swift:swift /srv/node
+
 # recon
 mkdir -p /var/cache/swift
 chown -R root:swift /var/cache/swift
 chmod -R 775 /var/cache/swift
-
 
 
 ## Storage node
