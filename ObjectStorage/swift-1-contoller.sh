afdda36644a611c987376b2cf077916c7b88dd32
shhan <shhan0226@gmail.com>
2023-03-15 15:48:03 +0900

    update swift

--- ObjectStorage/swift-1-contoller.sh
+++ ObjectStorage/swift-1-contoller.sh
@@ -25,15 +25,14 @@ echo "Swift Controller!!"
 ## Controller Node :
 . ../admin-openrc
 openstack user create --domain default --password ${STACK_PASSWD} swift
+
 openstack role add --project service --user swift admin
+
 openstack service create --name swift --description "OpenStack Object Storage" object-store
 
-openstack endpoint create --region RegionOne \
-  object-store public http://${SET_IP}:8080/v1/AUTH_%\(project_id\)s
-openstack endpoint create --region RegionOne \
-  object-store internal http://${SET_IP}:8080/v1/AUTH_%\(project_id\)s
-openstack endpoint create --region RegionOne \
-  object-store admin http://${SET_IP}:8080/v1
+openstack endpoint create --region RegionOne object-store public http://${SET_IP}:8080/v1/AUTH_%\(project_id\)s
+openstack endpoint create --region RegionOne object-store internal http://${SET_IP}:8080/v1/AUTH_%\(project_id\)s
+openstack endpoint create --region RegionOne object-store admin http://${SET_IP}:8080/v1
 
 
 ##################################
