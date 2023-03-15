4c8e9d5966a79d4f30b3bbadff4ac9f89595335a
shhan <shhan0226@gmail.com>
2023-03-15 18:24:46 +0900

    update swfit 0315

--- ObjectStorage/swift-1-contoller.sh
+++ ObjectStorage/swift-1-contoller.sh
@@ -24,11 +24,13 @@ echo "Swift Controller!!"
 
 ## Controller Node :
 . ../admin-openrc
+
 openstack user create --domain default --password ${STACK_PASSWD} swift
 
 openstack role add --project service --user swift admin
 
-openstack service create --name swift --description "OpenStack Object Storage" object-store
+openstack service create --name swift \
+  --description "OpenStack Object Storage" object-store
 
 openstack endpoint create --region RegionOne object-store public http://${SET_IP}:8080/v1/AUTH_%\(project_id\)s
 openstack endpoint create --region RegionOne object-store internal http://${SET_IP}:8080/v1/AUTH_%\(project_id\)s
@@ -40,24 +42,29 @@ openstack endpoint create --region RegionOne object-store admin http://${SET_IP}
 ##################################
 
 # install package
-apt-get install -y swift swift-proxy python3-swiftclient python3-keystoneclient python3-keystonemiddleware memcached python3-memcache
+apt-get install -y swift swift-proxy python3-swiftclient python3-keystoneclient python3-keystonemiddleware memcached python3-memcache python3-webob
 
 # proxy-server
-curl -o /etc/swift/proxy-server.conf https://opendev.org/openstack/swift/raw/branch/stable/xena/etc/proxy-server.conf-sample
+#curl -o /etc/swift/proxy-server.conf https://opendev.org/openstack/swift/raw/branch/stable/xena/etc/proxy-server.conf-sample
+curl -o /etc/swift/proxy-server.conf https://opendev.org/openstack/swift/raw/branch/master/etc/proxy-server.conf-sample
 
 crudini --set /etc/swift/proxy-server.conf DEFAULT bind_port 8080
 crudini --set /etc/swift/proxy-server.conf DEFAULT user swift
 crudini --set /etc/swift/proxy-server.conf DEFAULT swift_dir /etc/swift
+
 # 추가
-crudini --set /etc/swift/proxy-server.conf DEFAULT bind_ip 0.0.0.0
+#crudini --set /etc/swift/proxy-server.conf DEFAULT bind_ip 0.0.0.0
 
 crudini --set /etc/swift/proxy-server.conf pipeline:main pipeline "catch_errors gatekeeper healthcheck proxy-logging cache container_sync bulk ratelimit authtoken keystoneauth container-quotas account-quotas slo dlo versioned_writes proxy-logging proxy-server"
 
 crudini --set /etc/swift/proxy-server.conf app:proxy-server use egg:swift#proxy
-crudini --set /etc/swift/proxy-server.conf app:proxy-server allow_account_management True
+#crudini --set /etc/swift/proxy-server.conf app:proxy-server allow_account_management True
 crudini --set /etc/swift/proxy-server.conf app:proxy-server account_autocreate True
 
-crudini --set /etc/swift/proxy-server.conf filter:authtoken paste.filter_factory keystonemiddleware.auth_token:filter_factory
+crudini --set /etc/swift/proxy-server.conf filter:keystoneauth use egg:swift#keystoneauth
+crudini --set /etc/swift/proxy-server.conf filter:keystoneauth operator_roles admin,user,swift,member
+
+crudini --set /etc/swift/proxy-server.conf filter:authtoken "paste.filter_factory" "keystonemiddleware.auth_token:filter_factory"
 crudini --set /etc/swift/proxy-server.conf filter:authtoken www_authenticate_uri http://${SET_IP}:5000
 crudini --set /etc/swift/proxy-server.conf filter:authtoken auth_url http://${SET_IP}:5000
 crudini --set /etc/swift/proxy-server.conf filter:authtoken memcached_servers ${SET_IP}:11211
@@ -69,52 +76,118 @@ crudini --set /etc/swift/proxy-server.conf filter:authtoken username swift
 crudini --set /etc/swift/proxy-server.conf filter:authtoken password ${STACK_PASSWD}
 crudini --set /etc/swift/proxy-server.conf filter:authtoken delay_auth_decision True
 
-crudini --set /etc/swift/proxy-server.conf filter:keystoneauth use egg:swift#keystoneauth
-crudini --set /etc/swift/proxy-server.conf filter:keystoneauth operator_roles admin,user,swift
+
 
 #추가
-crudini --set /etc/swift/proxy-server.conf filter:healthcheck use egg:swift#healthcheck
+#crudini --set /etc/swift/proxy-server.conf filter:healthcheck use egg:swift#healthcheck
 
 crudini --set /etc/swift/proxy-server.conf filter:cache use egg:swift#memcache
 crudini --set /etc/swift/proxy-server.conf filter:cache memcache_servers ${SET_IP}:11211
 
 # 추가
-crudini --set /etc/swift/proxy-server.conf filter:ratelimit use egg:swift#ratelimit
+#crudini --set /etc/swift/proxy-server.conf filter:ratelimit use egg:swift#ratelimit
 
 # 추가
-crudini --set /etc/swift/proxy-server.conf filter:domain_remap use egg:swift#domain_remap
-crudini --set /etc/swift/proxy-server.conf filter:catch_errors use egg:swift#catch_errors
-crudini --set /etc/swift/proxy-server.conf filter:cname_lookup use egg:swift#cname_lookup
-crudini --set /etc/swift/proxy-server.conf filter:staticweb use egg:swift#staticweb
-crudini --set /etc/swift/proxy-server.conf filter:tempurl use egg:swift#tempurl
-crudini --set /etc/swift/proxy-server.conf filter:formpost use egg:swift#formpost
-crudini --set /etc/swift/proxy-server.conf filter:name_check use egg:swift#name_check
-crudini --set /etc/swift/proxy-server.conf filter:list-endpoints use egg:swift#list_endpoints
-crudini --set /etc/swift/proxy-server.conf filter:proxy-logging use egg:swift#proxy_logging
-crudini --set /etc/swift/proxy-server.conf filter:bulk use egg:swift#bulk
-crudini --set /etc/swift/proxy-server.conf filter:slo use egg:swift#slo
-crudini --set /etc/swift/proxy-server.conf filter:dlo use egg:swift#dlo
-crudini --set /etc/swift/proxy-server.conf filter:container-quotas use egg:swift#container_quotas
-crudini --set /etc/swift/proxy-server.conf filter:account-quotas use egg:swift#account_quotas
-crudini --set /etc/swift/proxy-server.conf filter:gatekeeper use egg:swift#gatekeeper
-crudini --set /etc/swift/proxy-server.conf filter:container_sync use egg:swift#container_sync
-crudini --set /etc/swift/proxy-server.conf filter:xprofile use egg:swift#xprofile
-crudini --set /etc/swift/proxy-server.conf filter:versioned_writes use egg:swift#versioned_writes
+#crudini --set /etc/swift/proxy-server.conf filter:domain_remap use egg:swift#domain_remap
+#crudini --set /etc/swift/proxy-server.conf filter:catch_errors use egg:swift#catch_errors
+#crudini --set /etc/swift/proxy-server.conf filter:cname_lookup use egg:swift#cname_lookup
+#crudini --set /etc/swift/proxy-server.conf filter:staticweb use egg:swift#staticweb
+#crudini --set /etc/swift/proxy-server.conf filter:tempurl use egg:swift#tempurl
+#crudini --set /etc/swift/proxy-server.conf filter:formpost use egg:swift#formpost
+#crudini --set /etc/swift/proxy-server.conf filter:name_check use egg:swift#name_check
+#crudini --set /etc/swift/proxy-server.conf filter:list-endpoints use egg:swift#list_endpoints
+#crudini --set /etc/swift/proxy-server.conf filter:proxy-logging use egg:swift#proxy_logging
+#crudini --set /etc/swift/proxy-server.conf filter:bulk use egg:swift#bulk
+#crudini --set /etc/swift/proxy-server.conf filter:slo use egg:swift#slo
+#crudini --set /etc/swift/proxy-server.conf filter:dlo use egg:swift#dlo
+#crudini --set /etc/swift/proxy-server.conf filter:container-quotas use egg:swift#container_quotas
+#crudini --set /etc/swift/proxy-server.conf filter:account-quotas use egg:swift#account_quotas
+#crudini --set /etc/swift/proxy-server.conf filter:gatekeeper use egg:swift#gatekeeper
+#crudini --set /etc/swift/proxy-server.conf filter:container_sync use egg:swift#container_sync
+#crudini --set /etc/swift/proxy-server.conf filter:xprofile use egg:swift#xprofile
+#crudini --set /etc/swift/proxy-server.conf filter:versioned_writes use egg:swift#versioned_writes
 
+
+
+##################################
+# Rings
+##################################
+## Controller node:
+
+# account
+swift-ring-builder /etc/swift/account.builder create 10 3 1
+#swift-ring-builder /etc/swift/account.builder add --region 1 --zone 1 --ip ${SET_IP} --port 6202 --device sdb --weight 100 
+#swift-ring-builder /etc/swift/account.builder add --region 1 --zone 1 --ip ${SET_IP} --port 6202 --device sdc --weight 100
+swift-ring-builder /etc/swift/account.builder add --region 1 --zone 1 --ip 192.168.2.53 --port 6202 --device sdb --weight 100 
+swift-ring-builder /etc/swift/account.builder add --region 1 --zone 1 --ip 192.168.2.53 --port 6202 --device sdc --weight 100 
+swift-ring-builder /etc/swift/account.builder add --region 1 --zone 2 --ip 192.168.2.54 --port 6202 --device sdb --weight 100 
+swift-ring-builder /etc/swift/account.builder add --region 1 --zone 2 --ip 192.168.2.54 --port 6202 --device sdc --weight 100 
+swift-ring-builder /etc/swift/account.builder
+swift-ring-builder /etc/swift/account.builder rebalance
+
+# container
+swift-ring-builder /etc/swift/container.builder create 10 3 1
+#swift-ring-builder /etc/swift/container.builder add --region 1 --zone 1 --ip ${SET_IP} --port 6201 --device sdb --weight 100
+#swift-ring-builder /etc/swift/container.builder add --region 1 --zone 1 --ip ${SET_IP} --port 6201 --device sdc --weight 100
+swift-ring-builder /etc/swift/container.builder add --region 1 --zone 1 --ip 192.168.2.53 --port 6201 --device sdb --weight 100
+swift-ring-builder /etc/swift/container.builder add --region 1 --zone 1 --ip 192.168.2.53 --port 6201 --device sdc --weight 100
+swift-ring-builder /etc/swift/container.builder add --region 1 --zone 2 --ip 192.168.2.54 --port 6201 --device sdb --weight 100
+swift-ring-builder /etc/swift/container.builder add --region 1 --zone 2 --ip 192.168.2.54 --port 6201 --device sdc --weight 100
+swift-ring-builder /etc/swift/container.builder
+swift-ring-builder /etc/swift/container.builder rebalance
+
+# object
+swift-ring-builder /etc/swift/object.builder create 10 3 1
+#swift-ring-builder /etc/swift/object.builder add --region 1 --zone 1 --ip ${SET_IP} --port 6200 --device sdb --weight 100
+#swift-ring-builder /etc/swift/object.builder add --region 1 --zone 1 --ip ${SET_IP} --port 6200 --device sdc --weight 100
+swift-ring-builder /etc/swift/object.builder add --region 1 --zone 1 --ip 192.168.2.53 --port 6200 --device sdb --weight 100
+swift-ring-builder /etc/swift/object.builder add --region 1 --zone 1 --ip 192.168.2.53 --port 6200 --device sdc --weight 100
+swift-ring-builder /etc/swift/object.builder add --region 1 --zone 2 --ip 192.168.2.54 --port 6200 --device sdb --weight 100
+swift-ring-builder /etc/swift/object.builder add --region 1 --zone 2 --ip 192.168.2.54 --port 6200 --device sdc --weight 100
+swift-ring-builder /etc/swift/object.builder
+swift-ring-builder /etc/swift/object.builder rebalance
+
+
+chown swift. /etc/swift/*.gz
+systemctl restart swift-proxy
+service memcached restart
+service swift-proxy restart
+
+ls /etc/swift
+
+
+echo "scp account.ring.gz, container.ring.gz, object.ring.gz"
+sudo apt-get install sshpass -y
+#echo -e "vraptor\nyes" | scp /etc/swift/*.gz vraptor@192.168.2.53:/home/vraptor
+sshpass -p "vraptor" scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null /etc/swift/*.gz vraptor@192.168.2.53:/home/vraptor
+
+#echo -e "vraptor\nyes" | scp /etc/swift/*.gz vraptor@192.168.2.54:/home/vraptor
+#scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null /etc/swift/*.gz vraptor@192.168.2.54:/home/vraptor
+sshpass -p "vraptor" scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null /etc/swift/*.gz vraptor@192.168.2.54:/home/vraptor
+
+
+
+
+
+##################################
+# Finalize installation
+##################################
 
 # /etc/swift/swift.conf
 curl -o /etc/swift/swift.conf https://opendev.org/openstack/swift/raw/branch/stable/${OPENSTACK_VER}/etc/swift.conf-sample
 
-crudini --set /etc/swift/swift.conf swift-hash swift_hash_path_suffix hash_suf_
-crudini --set /etc/swift/swift.conf swift-hash swift_hash_path_prefix hash_prf_
+crudini --set /etc/swift/swift.conf swift-hash swift_hash_path_suffix hash_swift_
+crudini --set /etc/swift/swift.conf swift-hash swift_hash_path_prefix hash_swift_
 
 chown -R root:swift /etc/swift
-
 echo "/etc/hosts \ 192.168.2.52 x86cinder \ 192.168.2.53 x86sw1 \ 192.168.2.54 x86sw2"
+
+service memcached restart
+service swift-proxy restart
 
 . ../admin-openrc
 openstack user list
 openstack service list
 openstack endpoint list | grep swift
 
-echo "SWIFT CONTROLLER INSTALLED ... END"
+echo "SWIFT INSTALLED ... END"
