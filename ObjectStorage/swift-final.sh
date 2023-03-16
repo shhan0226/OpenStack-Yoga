e403e4e10c1b1c20af83ce9da2e246f509ec0451
shhan <shhan0226@gmail.com>
2023-03-16 11:39:20 +0900

    update swfit 0316

--- ObjectStorage/swift-final.sh
+++ ObjectStorage/swift-final.sh
@@ -20,4 +20,20 @@ service memcached restart
 service swift-proxy restart
 swift stat
 
+
+echo "TEST CMD..."
+echo "openstack container create test-container"
+echo "openstack container list"
+echo "touch testfile.txt"
+echo "openstack object create test-container testfile.txt"
+echo "openstack object list test-container"
+echo "rm testfile.txt"
+echo "ls"
+echo "openstack object save test-container testfile.txt"
+echo "ls"
+echo "openstack object delete test-container testfile.txt"
+echo "openstack object list test-container"
+echo "openstack container delete test-container"
+echo "openstack container list"
+
 echo "SWIFT FINAL INSTALLED ... END"
