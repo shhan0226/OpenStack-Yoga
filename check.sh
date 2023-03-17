d87111eb435cd9c274949c40a1c14bec711c24bd
shhan <shhan0226@gmail.com>
2023-03-17 12:08:32 +0900

    update0317 check

--- check.sh
+++ check.sh
@@ -15,6 +15,7 @@ fi
 ##################################
 # admin check
 ##################################
+sync
 FILE_PATH="./admin-openrc"
 if [ ! -f "$FILE_PATH" ]; then
   echo "admin-openrc 파일이 존재하지 않습니다: $FILE_PATH"
@@ -27,8 +28,6 @@ echo "admin-openrc 파일이 존재합니다: $FILE_PATH"
 ##################################
 # nova check
 ##################################
-sync
-. admin-openrc
 openstack compute service list --service nova-compute
 su -s /bin/sh -c "nova-manage cell_v2 discover_hosts --verbose" nova
 crudini --set /etc/nova/nova.conf scheduler discover_hosts_in_cells_interval 300
@@ -38,7 +37,6 @@ nova-status upgrade check
 ##################################
 # service check
 ##################################
-
 openstack user list
 openstack service list
 openstack catalog list
