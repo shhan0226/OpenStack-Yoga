7deedccc6108e438e176668e6f8585fa764615bb
shhan <shhan0226@gmail.com>
2023-03-17 12:07:56 +0900

    update0317 check

--- check.sh
+++ check.sh
@@ -13,41 +13,32 @@ else
 fi
 
 ##################################
-# INPUT DATA PRINT
-##################################
-source /root/OpenStack-Yoga/set.conf
-echo "... set!!"
-
-##################################
 # admin check
 ##################################
-cat > admin-openrc << EOF
-export OS_PROJECT_DOMAIN_NAME=Default
-export OS_USER_DOMAIN_NAME=Default
-export OS_PROJECT_NAME=admin
-export OS_USERNAME=admin
-export OS_PASSWORD=stack
-export OS_AUTH_URL=http://${SET_IP}:5000/v3
-export OS_IDENTITY_API_VERSION=3
-export OS_IMAGE_API_VERSION=2
-EOF
+FILE_PATH="./admin-openrc"
+if [ ! -f "$FILE_PATH" ]; then
+  echo "admin-openrc 파일이 존재하지 않습니다: $FILE_PATH"
+  exit 1
+fi
+
+echo "admin-openrc 파일이 존재합니다: $FILE_PATH"
+. admin-openrc
 
 ##################################
 # nova check
 ##################################
 sync
+. admin-openrc
 openstack compute service list --service nova-compute
 su -s /bin/sh -c "nova-manage cell_v2 discover_hosts --verbose" nova
 crudini --set /etc/nova/nova.conf scheduler discover_hosts_in_cells_interval 300
-# NOVA Verify operation
-. admin-openrc
 openstack compute service list
 nova-status upgrade check
 
 ##################################
 # service check
 ##################################
-. admin-openrc
+
 openstack user list
 openstack service list
 openstack catalog list
