5d665a29d0b841c41d2fcf4970d1e389220c6465
shhan <shhan0226@gmail.com>
2023-06-12 11:48:10 +0900

    update 2023.06.12

--- Operation/set_Instance.sh
+++ Operation/set_Instance.sh
@@ -17,8 +17,8 @@ fi
 ##################################
 sync
 source admin-openrc
-read -p "NAME_INST? {yes|no|ENTER=no} :" NAME_INST
-openstack server create --image ubuntu2004 --flavor flavor1 --key-name arm-key --network internal --user-data init.sh --security-group arm-secu ${NAME_INST}
+read -p "INPUT INSTANCE NAME? :" NAME_INST
+openstack server create --image ubuntu2004 --flavor flavor1 --key-name arm-key --network internal --user-data init.sh --security-group arm-secu ${NAME_INSTANCE}
 ##################################
 # add floating IP
 ##################################
@@ -27,11 +27,6 @@ source admin-openrc
 . demo-openrc
 openstack floating ip create external
 openstack floating ip list --fit-width
-read -p "IF SET? {yes|no|ENTER=no} :" CHECKER_IF
-if [ "$CHECKER_IF" = "yes" ]; then
-  read -p "INPUT IF? :" INPUT_IF
-  openstack server add floating ip ${NAME_INST} ${INPUT_IF}
-else
-  echo "IF no SET ..."
-fi
+read -p "INPUT IP SET? :" INPUT_IP
+openstack server add floating ip ${NAME_INSTANCE} ${INPUT_IP}
 openstack floating ip list
